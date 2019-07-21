8f07c2e07f5488fb9529109c203d096bae80e9c0
Kritika <kritika991@gmail.com>
2019-07-21 13:23:42 +0200

    deleted

---
+++ src/samplepackage/Assignment1.java
@@ -0,0 +1,15 @@
+package samplepackage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment1  {
+
+	public static void main(String[] args)  throws InterruptedException {
+		
+		System.out.println("Hello");
+
+	}
+
+}
