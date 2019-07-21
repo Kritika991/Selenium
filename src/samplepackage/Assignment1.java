87123a9d2c0991070b155bf345d17afb031e1670
Kritika <kritika991@gmail.com>
2019-07-21 13:27:44 +0200

    testng

--- src/samplepackage/Assignment1.java
+++ src/samplepackage/Assignment1.java
@@ -3,13 +3,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Assignment1  {
 
-	public static void main(String[] args)  throws InterruptedException {
-		
+	
+		@Test
+		public void message()
+		{
 		System.out.println("Hello");
+		}
 
-	}
+	
 
 }
